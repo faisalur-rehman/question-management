5be20fa41bdaf67e87644c37f3dfa403d855ce21
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-05-28 14:05:29 +0500

    Changed  screen 6

--- src/components/PresenterLiveQuestions/PresenterLiveQuestions.js
+++ src/components/PresenterLiveQuestions/PresenterLiveQuestions.js
@@ -4,38 +4,48 @@ import PresenterLiveQuestionCard from "./PresenterLiveQuestionCard";
 
 const PresenterLiveQuestions = ({ noQuestion }) => {
   return (
-    <div style={{ marginLeft: 10 }}>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <h2 className="incoming">LIVE Questions</h2>
-      <div className="live-header bg-presenter">
-        {!noQuestion ? (
-          <div
-            className="remark"
-            style={{
-              backgroundColor: "white",
-            }}
-          >
-            <i className="far fa-check-circle remark-tick"></i>
-          </div>
-        ) : (
-          <div></div>
-        )}
-        <button className="IncomingQuestionCard-btn2 light">
-          <i className="fa fa-user-circle"></i>
-          Presenter
-        </button>
-      </div>
-      <div className="archeived-quesiton-panel">
-        {!noQuestion ? (
-          <>
-            <PresenterLiveQuestionCard />
-            <PresenterLiveQuestionCard />
-            <PresenterLiveQuestionCard />
-            <PresenterLiveQuestionCard />
-            <PresenterLiveQuestionCard />
-          </>
-        ) : (
-          <div className="no-question-presenter"></div>
-        )}
+      <div
+        style={{
+          marginLeft: 10,
+          maxWidth: "500px",
+          minWidth: "450px",
+          minHeight: "97vh",
+        }}
+        className="live-presenter"
+      >
+        <div className="live-header bg-presenter">
+          {!noQuestion ? (
+            <div
+              className="remark"
+              style={{
+                backgroundColor: "white",
+              }}
+            >
+              <i className="far fa-check-circle remark-tick"></i>
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <button className="IncomingQuestionCard-btn2 light">
+            <i className="fa fa-user-circle"></i>
+            Presenter
+          </button>
+        </div>
+        <div className="archeived-quesiton-panel">
+          {!noQuestion ? (
+            <>
+              <PresenterLiveQuestionCard />
+              <PresenterLiveQuestionCard />
+              <PresenterLiveQuestionCard />
+              <PresenterLiveQuestionCard />
+              <PresenterLiveQuestionCard />
+            </>
+          ) : (
+            <div className="no-question-presenter"></div>
+          )}
+        </div>
       </div>
     </div>
   );
