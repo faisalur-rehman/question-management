c8d22976e72a2ae6afc9a235d25f5d0c76d7384b
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-05-28 11:09:15 +0500

    Updated screen 5 as per client requirement

--- src/components/NewProject/NewProject.js
+++ src/components/NewProject/NewProject.js
@@ -13,29 +13,29 @@ const NewProject = () => {
         <div className="new-project-card">
           <label>Project Title</label>
           <br />
-          <Input name="title" className="form-control input" />
+          <Input name="title" className="form-control project-input" />
           <p className="link">https://medquest.com(viewtype)</p>
           <label>Date</label>
           <br />
-          <Input name="title" className="form-control input date" />
+          <Input name="title" className="form-control project-input date" />
           <MdDateRange className="date-icon" />
           <div className="time-details">
             <div>
               <label>Start Time</label>
               <br />
-              <Input name="title" className="form-control input time" />
+              <Input name="title" className="form-control project-input time" />
               <BiTimeFive className="time-icon" />
             </div>
             <div>
               <label>End Time</label>
               <br />
-              <Input name="title" className="form-control input time" />
+              <Input name="title" className="form-control project-input time" />
               <BiTimeFive className="time-icon" />
             </div>
           </div>
           <label>Project Passcode</label>
           <br />
-          <Input name="passcode" className="form-control input" />
+          <Input name="passcode" className="form-control project-input" />
         </div>
         <div style={{ width: "35%" }}>
           <button color="primary" className="save-btn text-size active">
