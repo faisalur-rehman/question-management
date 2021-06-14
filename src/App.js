97083223fac4cdec1dd3aafe7a406937675329fd
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-06-14 08:32:40 +0500

    Added modals for question and remark

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
 import PresenterView from "./components/PresenterView/PresenterView";
 import AlarmScreen from "./components/AlarmScreen/AlarmScreen";
+import TransitionsModal from "./components/Modal/Modal";
 
 function App() {
   return (
@@ -258,6 +259,93 @@ function App() {
             <div className="body">
               <div className="sidebar-header">
                 <AlarmScreen />
+              </div>
+            </div>
+          </Route>
+          <Route path="/screen24">
+            <div className="body">
+              <DashboardHeader />
+              <TransitionsModal
+                modalType="Add Question"
+                modal={
+                  <section className="question">
+                    <h2>Question</h2>
+                    <form action="" className="form">
+                      <div className="form-group">
+                        <label for="pasteName">Paste Name</label>
+                        <input type="text" name="pasteName" />
+                      </div>
+                      <div className="form-group">
+                        <label for="pasteQuestion">Paste Question</label>
+                        <textarea
+                          name="pasteQuestion"
+                          id="pasteQuestion"
+                        ></textarea>
+                      </div>
+                      <div className="btn-group">
+                        <div className="left-btn">
+                          <button>
+                            <i className="fa fa-save"></i>
+                            Save
+                          </button>
+                        </div>
+                        <div className="right-btn">
+                          <button className="right-btn1">
+                            <i className="fa fa-check-double"></i>
+                            Moderator
+                          </button>
+                          <button className="right-btn2">
+                            <i className="fa fa-user-circle"></i>
+                            Presenter
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </section>
+                }
+              />
+              <TransitionsModal
+                modalType="Add Remarks"
+                modal={
+                  <section className="remarks">
+                    <h2>Remarks</h2>
+                    <form action="" className="form">
+                      <div className="form-group">
+                        <label for="opmerkingPlaatsen">
+                          Opmerking plaatsen
+                        </label>
+                        <textarea
+                          name="opmerkingPlaatsen"
+                          id="opmerkingPlaatsen"
+                        ></textarea>
+                      </div>
+                      <div className="btn-group">
+                        <div className="left-btn">
+                          <button>
+                            <i className="fa fa-save"></i>
+                            Save
+                          </button>
+                        </div>
+                        <div className="right-btn">
+                          <button className="right-btn1">
+                            <i className="fa fa-pen"></i>
+                            Modirator
+                          </button>
+                        </div>
+                      </div>
+                    </form>
+                  </section>
+                }
+              />
+              <div className="sidebar-header">
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <SavedRemarks />
+                  <Questions />
+                </div>
+                <div style={{ display: "flex", flexDirection: "column" }}>
+                  <IncomingQuestion />
+                </div>
+                <LiveQuestions />
               </div>
             </div>
           </Route>
--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -5,37 +5,6 @@ const Questions = (props) => {
   let bg = props.alarmActive ? "bg" : "";
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <section className="question">
-        <h2>Question</h2>
-        <form action="" className="form">
-          <div className="form-group">
-            <label for="pasteName">Paste Name</label>
-            <input type="text" name="pasteName" />
-          </div>
-          <div className="form-group">
-            <label for="pasteQuestion">Paste Question</label>
-            <textarea name="pasteQuestion" id="pasteQuestion"></textarea>
-          </div>
-          <div className="btn-group">
-            <div className="left-btn">
-              <button>
-                <i className="fa fa-save"></i>
-                Save
-              </button>
-            </div>
-            <div className="right-btn">
-              <button className="right-btn1">
-                <i className="fa fa-check-double"></i>
-                Moderator
-              </button>
-              <button className="right-btn2">
-                <i className="fa fa-user-circle"></i>
-                Presenter
-              </button>
-            </div>
-          </div>
-        </form>
-      </section>
       <section className="alarm">
         <h2>Presenter Alarm</h2>
         <div className="alarm-btn-group">
@@ -52,32 +21,6 @@ const Questions = (props) => {
             Beeld Alarm
           </button>
         </div>
-      </section>
-      <section className="remarks">
-        <h2>Remarks</h2>
-        <form action="" className="form">
-          <div className="form-group">
-            <label for="opmerkingPlaatsen">Opmerking plaatsen</label>
-            <textarea
-              name="opmerkingPlaatsen"
-              id="opmerkingPlaatsen"
-            ></textarea>
-          </div>
-          <div className="btn-group">
-            <div className="left-btn">
-              <button>
-                <i className="fa fa-save"></i>
-                Save
-              </button>
-            </div>
-            <div className="right-btn">
-              <button className="right-btn1">
-                <i className="fa fa-pen"></i>
-                Modirator
-              </button>
-            </div>
-          </div>
-        </form>
       </section>
     </div>
   );
