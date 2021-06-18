91801c6545d615bcaf222255024148256d8bf3db
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-18 07:36:06 +0500

    code refactor: move create question to sepeate file ..

--- src/screens/Questions.jsx
+++ src/screens/Questions.jsx
@@ -1,15 +1,10 @@
-import { useEffect, useState } from "react";
-import { Field } from "formik";
-
 import DashboardHeader from "../components/DasboardHeader/DashboardHeader";
 import TransitionsModal from "../components/Modal/Modal";
 import SavedRemarks from "../components/SavedRemarks/SavedRemarks";
 import Questions from "../components/Questions/Questions";
 import IncomingQuestion from "../components/IncomingQuestion/IncomingQuestion";
 import LiveQuestions from "../components/LiveQuestions/LiveQuestions";
-import AppForm from "../common/AppForm";
-import { createQuestionSchema } from "../utils/validations";
-import { socket } from "../apis/socket-connect";
+import CreateQuestion from "../widgets/question/CreateQuestion";
 
 export default function QuestionsScreen() {
   return (
@@ -60,102 +55,3 @@ export default function QuestionsScreen() {
     </div>
   );
 }
-
-function CreateQuestion() {
-  const [questionSelected, setQuestionSelected] = useState();
-
-  useEffect(() => {
-    socket.on("exception", (errors) => {
-      console.log("socket errors", errors);
-    });
-  }, []);
-
-  const handleSubmit = ({ formValues }) => {
-    console.log("questionSelected", questionSelected);
-
-    const project = "60cbe95e130a2e1045a84d55";
-    const formFields = { ...formValues, project };
-
-    const url = getURLForCreatingQuestions();
-    console.log("url", url);
-
-    socket.emit(url, { ...formFields }, (data) => {
-      console.log("socket incoming question", data);
-    });
-  };
-
-  const getURLForCreatingQuestions = () => {
-    let url = "create-incoming-question";
-
-    if (questionSelected === "moderator") {
-      url = "create-moderator-question";
-    } else if (questionSelected === "presenter") {
-      url = "create-presenter-question";
-    }
-
-    return url;
-  };
-
-  return (
-    <TransitionsModal
-      modalType="Add Question"
-      modal={
-        <section className="question">
-          <h2>Question</h2>
-          <AppForm
-            initialValues={questionInitialValue}
-            validationSchema={createQuestionSchema}
-            handleSubmit={handleSubmit}
-          >
-            <div className="form">
-              <div className="form-group">
-                <label for="pasteName">Paste Name</label>
-                {/* <input type="text" name="name" /> */}
-                <Field type="text" name="name" />
-              </div>
-              <div className="form-group">
-                <label for="pasteQuestion">Paste Question</label>
-                {/* <textarea name="pasteQuestion" id="pasteQuestion"></textarea> */}
-                <Field as="textarea" name="questionText" />
-              </div>
-              <div className="btn-group">
-                <div className="left-btn">
-                  <button
-                    type="submit"
-                    onClick={() => setQuestionSelected("save")}
-                  >
-                    <i className="fa fa-save"></i>
-                    Save
-                  </button>
-                </div>
-                <div className="right-btn">
-                  <button
-                    className="right-btn1"
-                    type="submit"
-                    onClick={() => setQuestionSelected("moderator")}
-                  >
-                    <i className="fa fa-check-double"></i>
-                    Moderator
-                  </button>
-                  <button
-                    className="right-btn2"
-                    type="submit"
-                    onClick={() => setQuestionSelected("presenter")}
-                  >
-                    <i className="fa fa-user-circle"></i>
-                    Presenter
-                  </button>
-                </div>
-              </div>
-            </div>
-          </AppForm>
-        </section>
-      }
-    />
-  );
-}
-
-const questionInitialValue = {
-  name: "",
-  questionText: "",
-};
