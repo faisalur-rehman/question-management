a3b1046bd8ebf4c39645c30f3a53f18b4d3897b2
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-18 06:41:59 +0500

    added questions screen for displaying live questions, incoming questions, saved remarks and so on ..

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import Login from "./components/Login/Login";
 import PresenterView from "./components/PresenterView/PresenterView";
 import AlarmScreen from "./components/AlarmScreen/AlarmScreen";
 import TransitionsModal from "./components/Modal/Modal";
+import QuestionsScreen from "./screens/Questions";
 
 function App() {
   return (
@@ -41,7 +42,7 @@ function App() {
               <Projects />
             </div>
           </Route>
-          <Route path="/screen4">
+          <Route path="/permissions">
             <div className="sidebar-header body">
               <SideBar />
               <ProfileSetting />
@@ -263,91 +264,7 @@ function App() {
             </div>
           </Route>
           <Route path="/screen24">
-            <div className="body">
-              <DashboardHeader />
-              <TransitionsModal
-                modalType="Add Question"
-                modal={
-                  <section className="question">
-                    <h2>Question</h2>
-                    <form action="" className="form">
-                      <div className="form-group">
-                        <label for="pasteName">Paste Name</label>
-                        <input type="text" name="pasteName" />
-                      </div>
-                      <div className="form-group">
-                        <label for="pasteQuestion">Paste Question</label>
-                        <textarea
-                          name="pasteQuestion"
-                          id="pasteQuestion"
-                        ></textarea>
-                      </div>
-                      <div className="btn-group">
-                        <div className="left-btn">
-                          <button>
-                            <i className="fa fa-save"></i>
-                            Save
-                          </button>
-                        </div>
-                        <div className="right-btn">
-                          <button className="right-btn1">
-                            <i className="fa fa-check-double"></i>
-                            Moderator
-                          </button>
-                          <button className="right-btn2">
-                            <i className="fa fa-user-circle"></i>
-                            Presenter
-                          </button>
-                        </div>
-                      </div>
-                    </form>
-                  </section>
-                }
-              />
-              <TransitionsModal
-                modalType="Add Remarks"
-                modal={
-                  <section className="remarks">
-                    <h2>Remarks</h2>
-                    <form action="" className="form">
-                      <div className="form-group">
-                        <label for="opmerkingPlaatsen">
-                          Opmerking plaatsen
-                        </label>
-                        <textarea
-                          name="opmerkingPlaatsen"
-                          id="opmerkingPlaatsen"
-                        ></textarea>
-                      </div>
-                      <div className="btn-group">
-                        <div className="left-btn">
-                          <button>
-                            <i className="fa fa-save"></i>
-                            Save
-                          </button>
-                        </div>
-                        <div className="right-btn">
-                          <button className="right-btn1">
-                            <i className="fa fa-pen"></i>
-                            Modirator
-                          </button>
-                        </div>
-                      </div>
-                    </form>
-                  </section>
-                }
-              />
-              <div className="sidebar-header">
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                  <SavedRemarks />
-                  <Questions />
-                </div>
-                <div style={{ display: "flex", flexDirection: "column" }}>
-                  <IncomingQuestion />
-                </div>
-                <LiveQuestions />
-              </div>
-            </div>
+            <QuestionsScreen />
           </Route>
         </div>
       </Switch>
---
+++ src/screens/Questions.jsx
@@ -0,0 +1,91 @@
+import DashboardHeader from "../components/DasboardHeader/DashboardHeader";
+import TransitionsModal from "../components/Modal/Modal";
+import SavedRemarks from "../components/SavedRemarks/SavedRemarks";
+import Questions from "../components/Questions/Questions";
+import IncomingQuestion from "../components/IncomingQuestion/IncomingQuestion";
+import LiveQuestions from "../components/LiveQuestions/LiveQuestions";
+
+export default function QuestionsScreen() {
+  return (
+    <div className="body">
+      <DashboardHeader />
+      <TransitionsModal
+        modalType="Add Question"
+        modal={
+          <section className="question">
+            <h2>Question</h2>
+            <form action="" className="form">
+              <div className="form-group">
+                <label for="pasteName">Paste Name</label>
+                <input type="text" name="pasteName" />
+              </div>
+              <div className="form-group">
+                <label for="pasteQuestion">Paste Question</label>
+                <textarea name="pasteQuestion" id="pasteQuestion"></textarea>
+              </div>
+              <div className="btn-group">
+                <div className="left-btn">
+                  <button>
+                    <i className="fa fa-save"></i>
+                    Save
+                  </button>
+                </div>
+                <div className="right-btn">
+                  <button className="right-btn1">
+                    <i className="fa fa-check-double"></i>
+                    Moderator
+                  </button>
+                  <button className="right-btn2">
+                    <i className="fa fa-user-circle"></i>
+                    Presenter
+                  </button>
+                </div>
+              </div>
+            </form>
+          </section>
+        }
+      />
+      <TransitionsModal
+        modalType="Add Remarks"
+        modal={
+          <section className="remarks">
+            <h2>Remarks</h2>
+            <form action="" className="form">
+              <div className="form-group">
+                <label for="opmerkingPlaatsen">Opmerking plaatsen</label>
+                <textarea
+                  name="opmerkingPlaatsen"
+                  id="opmerkingPlaatsen"
+                ></textarea>
+              </div>
+              <div className="btn-group">
+                <div className="left-btn">
+                  <button>
+                    <i className="fa fa-save"></i>
+                    Save
+                  </button>
+                </div>
+                <div className="right-btn">
+                  <button className="right-btn1">
+                    <i className="fa fa-pen"></i>
+                    Modirator
+                  </button>
+                </div>
+              </div>
+            </form>
+          </section>
+        }
+      />
+      <div className="sidebar-header">
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <SavedRemarks />
+          <Questions />
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <IncomingQuestion />
+        </div>
+        <LiveQuestions />
+      </div>
+    </div>
+  );
+}
