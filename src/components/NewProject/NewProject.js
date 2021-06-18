f8bc37720ff7bb68b54d3a1a0a322f804fa83130
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-18 05:37:08 +0500

    code refactor: move create project form to a seperate component ..

--- src/components/NewProject/NewProject.js
+++ src/components/NewProject/NewProject.js
@@ -1,4 +1,4 @@
-import { Input } from "reactstrap";
+// import { Input } from "reactstrap";
 import { MdDateRange } from "react-icons/md";
 import { BiTimeFive } from "react-icons/bi";
 import { Field } from "formik";
@@ -24,76 +24,82 @@ const NewProject = () => {
 
   return (
     <div style={{ width: "90%" }}>
-      <AppForm
-        initialValues={initialValue}
-        validationSchema={projectSchema}
-        handleSubmit={handleCreateProject}
-      >
-        <div className="new-project">
-          <Header heading="New Project" search={false} />
-          <div className="new-project-card">
-            <label>Project Title</label>
-            <br />
-            <Field name="title" className="form-control project-input" />
-            <p className="link">https://medquest.com(viewtype)</p>
-            <label>Date</label>
-            <br />
-            {/* <input
-              type="date"
-              name="title"
-              className="form-control project-input date"
-            /> */}
-            <Field
-              type="date"
-              name="scheduleDate"
-              className="form-control project-input date"
-            />
-
-            <MdDateRange className="date-icon" />
-            <div className="time-details">
-              <div>
-                <label>Start Time</label>
-                <br />
-                {/* <input
-                  type="time"
-                  name="title"
-                  className="form-control project-input time"
-                /> */}
-                <Field
-                  type="time"
-                  name="startTime"
-                  className="form-control project-input time"
-                />
-                <BiTimeFive className="time-icon" />
-              </div>
-              <div>
-                <label>End Time</label>
-                <br />
-                <Field
-                  type="time"
-                  name="endTime"
-                  className="form-control project-input time"
-                />
-                <BiTimeFive className="time-icon" />
-              </div>
-            </div>
-          </div>
-          <div style={{ width: "35%" }}>
-            <button
-              color="primary"
-              className="save-btn text-size active"
-              type="submit"
-            >
-              Save
-            </button>
-          </div>
-        </div>
-      </AppForm>
+      <NewProjectForm onSubmit={handleCreateProject} />
     </div>
   );
 };
 
 export default NewProject;
+
+function NewProjectForm({ onSubmit }) {
+  return (
+    <AppForm
+      initialValues={initialValue}
+      validationSchema={projectSchema}
+      handleSubmit={onSubmit}
+    >
+      <div className="new-project">
+        <Header heading="New Project" search={false} />
+        <div className="new-project-card">
+          <label>Project Title</label>
+          <br />
+          <Field name="title" className="form-control project-input" />
+          <p className="link">https://medquest.com(viewtype)</p>
+          <label>Date</label>
+          <br />
+          {/* <input
+              type="date"
+              name="title"
+              className="form-control project-input date"
+            /> */}
+          <Field
+            type="date"
+            name="scheduleDate"
+            className="form-control project-input date"
+          />
+
+          <MdDateRange className="date-icon" />
+          <div className="time-details">
+            <div>
+              <label>Start Time</label>
+              <br />
+              {/* <input
+                  type="time"
+                  name="title"
+                  className="form-control project-input time"
+                /> */}
+              <Field
+                type="time"
+                name="startTime"
+                className="form-control project-input time"
+              />
+              <BiTimeFive className="time-icon" />
+            </div>
+            <div>
+              <label>End Time</label>
+              <br />
+              <Field
+                type="time"
+                name="endTime"
+                className="form-control project-input time"
+              />
+              <BiTimeFive className="time-icon" />
+            </div>
+          </div>
+        </div>
+        <div style={{ width: "35%" }}>
+          <button
+            color="primary"
+            className="save-btn text-size active"
+            type="submit"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </AppForm>
+  );
+}
 
 const initialValue = {
   title: "",
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,15 +3,22 @@ import "./Projects.css";
 import "../Header/Header.css";
 import ProjectName from "./ProjectName";
 import Header from "../Header/Header";
+import { useHistory } from "react-router-dom";
 // import Projects from "../Projects/Projects";
 
 const Projects = () => {
+  const history = useHistory();
+
+  const navigateToCreateProject = () => {
+    history.push("/create-project");
+  };
+
   return (
     <div style={{ margin: 30, width: "90%" }}>
       <Header heading="Projects" search={true} />
       <div className="projects">
         <div className="add-project">
-          <button>
+          <button onClick={navigateToCreateProject}>
             <span>+</span>New Project
           </button>
         </div>
