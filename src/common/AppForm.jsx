efc890e2ae5f2daf005ed6691c1b37c0135f7216
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-18 05:25:12 +0500

    getting form values after validating all fields when creating new project ...

---
+++ src/common/AppForm.jsx
@@ -0,0 +1,23 @@
+import { Formik } from "formik";
+
+const AppForm = ({
+  children,
+  initialValues,
+  validationSchema,
+  handleSubmit,
+}) => {
+  return (
+    <Formik
+      enableReinitialize
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        handleSubmit({ formValues: values });
+      }}
+    >
+      {(props) => <form onSubmit={props.handleSubmit}>{children}</form>}
+    </Formik>
+  );
+};
+
+export default AppForm;
--- src/components/NewProject/NewProject.js
+++ src/components/NewProject/NewProject.js
@@ -1,62 +1,99 @@
 import { Input } from "reactstrap";
 import { MdDateRange } from "react-icons/md";
 import { BiTimeFive } from "react-icons/bi";
+import { Field } from "formik";
 
 import Header from "../Header/Header";
 import "./NewProject.css";
 import * as projectApi from "../../apis/project";
 import useApi from "../../hooks/useApi";
+import AppForm from "../../common/AppForm";
+import { projectSchema } from "../../utils/validations";
 
 const NewProject = () => {
   const project = useApi(projectApi.createProject);
 
-  const handleCreateProject = async () => {
-    project.request({});
+  const handleCreateProject = async ({ formValues }) => {
+    console.log(formValues);
+    // project.request({});
   };
 
   return (
     <div style={{ width: "90%" }}>
-      <div className="new-project">
-        <Header heading="New Project" search={false} />
-        <div className="new-project-card">
-          <label>Project Title</label>
-          <br />
-          <Input name="title" className="form-control project-input" />
-          <p className="link">https://medquest.com(viewtype)</p>
-          <label>Date</label>
-          <br />
-          <input name="title" className="form-control project-input date" />
-          <MdDateRange className="date-icon" />
-          <div className="time-details">
-            <div>
-              <label>Start Time</label>
-              <br />
-              <Input name="title" className="form-control project-input time" />
-              <BiTimeFive className="time-icon" />
-            </div>
-            <div>
-              <label>End Time</label>
-              <br />
-              <Input name="title" className="form-control project-input time" />
-              <BiTimeFive className="time-icon" />
+      <AppForm
+        initialValues={initialValue}
+        validationSchema={projectSchema}
+        handleSubmit={handleCreateProject}
+      >
+        <div className="new-project">
+          <Header heading="New Project" search={false} />
+          <div className="new-project-card">
+            <label>Project Title</label>
+            <br />
+            <Field name="title" className="form-control project-input" />
+            <p className="link">https://medquest.com(viewtype)</p>
+            <label>Date</label>
+            <br />
+            {/* <input
+              type="date"
+              name="title"
+              className="form-control project-input date"
+            /> */}
+            <Field
+              type="date"
+              name="scheduleDate"
+              className="form-control project-input date"
+            />
+
+            <MdDateRange className="date-icon" />
+            <div className="time-details">
+              <div>
+                <label>Start Time</label>
+                <br />
+                {/* <input
+                  type="time"
+                  name="title"
+                  className="form-control project-input time"
+                /> */}
+                <Field
+                  type="time"
+                  name="startTime"
+                  className="form-control project-input time"
+                />
+                <BiTimeFive className="time-icon" />
+              </div>
+              <div>
+                <label>End Time</label>
+                <br />
+                <Field
+                  type="time"
+                  name="endTime"
+                  className="form-control project-input time"
+                />
+                <BiTimeFive className="time-icon" />
+              </div>
             </div>
           </div>
-          <label>Project Passcode</label>
-          <br />
-          <Input name="passcode" className="form-control project-input" />
+          <div style={{ width: "35%" }}>
+            <button
+              color="primary"
+              className="save-btn text-size active"
+              type="submit"
+            >
+              Save
+            </button>
+          </div>
         </div>
-        <div style={{ width: "35%" }}>
-          <button
-            color="primary"
-            className="save-btn text-size active"
-            onClick={handleCreateProject}
-          >
-            Save
-          </button>
-        </div>
-      </div>
+      </AppForm>
     </div>
   );
 };
 
 export default NewProject;
+
+const initialValue = {
+  title: "",
+  scheduleDate: "",
+  startTime: "",
+  endTime: "",
+};
