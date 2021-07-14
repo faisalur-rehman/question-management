25f4731c5fff7ee3cc37d58aa59194c5117c5284
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-07-15 00:36:31 +0500

    Added edit question for moderator

--- src/components/LiveQuestions/LiveQuestionCard.js
+++ src/components/LiveQuestions/LiveQuestionCard.js
@@ -1,28 +1,30 @@
 import React, { useState } from "react";
 import "../../Assets/css/main.css";
 import { socket } from "../../apis/socket-connect";
-import { makeStyles } from "@material-ui/core/styles";
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Fade from "@material-ui/core/Fade";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Modal from "@material-ui/core/Modal";
+// import Backdrop from "@material-ui/core/Backdrop";
+// import Fade from "@material-ui/core/Fade";
+import EditQuestionModal from "../Modal/EditQuestionModal";
 
-const useStyles = makeStyles((theme) => ({
-  modal: {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    border: "2px solid #000",
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   modal: {
+//     display: "flex",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   paper: {
+//     backgroundColor: theme.palette.background.paper,
+//     border: "2px solid #000",
+//     boxShadow: theme.shadows[5],
+//     padding: theme.spacing(2, 4, 3),
+//   },
+// }));
 
 const LiveQuestionCard = ({ question }) => {
-  const classes = useStyles();
+  // const classes = useStyles();
   const [clicked, setClicked] = useState(false);
+
   const [editQuestion, setEditQuestion] = useState(false);
   let show = !clicked ? "menu-icon-none" : "";
 
@@ -62,6 +64,7 @@ const LiveQuestionCard = ({ question }) => {
               fontSize: ".7rem",
               marginLeft: 5,
             }}
+            onClick={() => setEditQuestion(true)}
           ></i>
           <span className={`${show} menu-icon`}>
             <i
@@ -91,40 +94,11 @@ const LiveQuestionCard = ({ question }) => {
       <div className="card-body">
         <p>{question.questionText}</p>
       </div>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={editQuestion}
-        onClose={() => setEditQuestion(false)}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={editQuestion}>
-          <div className={classes.paper}>
-            <form action="" className="form">
-              <div className="form-group">
-                <label for="opmerkingPlaatsen">Edit Question</label>
-                <textarea
-                  name="opmerkingPlaatsen"
-                  id="opmerkingPlaatsen"
-                ></textarea>
-              </div>
-              <div className="btn-group">
-                <div className="left-btn">
-                  <button>
-                    <i className="fa fa-save"></i>
-                    Save
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </Fade>
-      </Modal>
+      <EditQuestionModal
+        show={editQuestion}
+        onClose={setEditQuestion}
+        question={question}
+      />
     </section>
   );
 };
