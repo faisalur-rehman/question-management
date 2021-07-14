b320dbb981913ca3e4dfe3df8acede20a323e407
faisalur-rehman <66237466+faisalur-rehman@users.noreply.github.com>
2021-07-14 16:09:41 +0500

    Added sockets for incoming question component

--- src/components/IncomingQuestion/IncomingQuestion.js
+++ src/components/IncomingQuestion/IncomingQuestion.js
@@ -1,14 +1,14 @@
 import "../../Assets/css/main.css";
 import IncomingQuestionCard from "./IncomingQuestionCard";
 import "./IncomingQuestions.css";
-
 import AppLoading from "../../common/AppLoading";
 import useFetchQuestions from "../../hooks/useFetchQuestions";
 
 const IncomingQuestion = () => {
   const { questions, isLoading } = useFetchQuestions(
     "all-incoming-questions",
-    "new-incoming-question"
+    "new-incoming-question",
+    "updated-incoming-questions"
   );
 
   if (isLoading) {
@@ -22,7 +22,7 @@ const IncomingQuestion = () => {
         {questions.length === 0 && <NoQuestion />}
 
         {questions.map((question) => (
-          <IncomingQuestionCard key={question._id} />
+          <IncomingQuestionCard key={question._id} question={question} />
         ))}
       </div>
     </div>
--- src/components/IncomingQuestion/IncomingQuestionCard.js
+++ src/components/IncomingQuestion/IncomingQuestionCard.js
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
 import "../../Assets/css/main.css";
 import archive_icon from "../../Assets/archive_icon.svg";
+import { socket } from "../../apis/socket-connect";
 
-const IncomingQuestionCard = () => {
+const IncomingQuestionCard = (props) => {
   const [clicked, setClicked] = useState(false);
   let show = !clicked ? "menu-icon-none" : "";
+
+  function handleIncomingQuestion(url) {
+    socket.emit(
+      url,
+      {
+        questionId: props.question._id,
+        projectId: "60e53f6ce35eb82ea8585cf5",
+      },
+      (data) => {
+        console.log(data);
+      }
+    );
+  }
+
   return (
     <section className="IncomingQuestionCard">
       <div className="card-header">
@@ -21,7 +36,10 @@ const IncomingQuestionCard = () => {
             className={`${show}`}
           />
           <span className={`${show} menu-icon`}>
-            <i class="fal fa-minus-circle" style={{}}></i>
+            <i
+              className="fal fa-minus-circle"
+              onClick={() => handleIncomingQuestion("delete-incoming-question")}
+            ></i>
           </span>
         </div>
         <div>
@@ -29,23 +47,28 @@ const IncomingQuestionCard = () => {
           <h5>Hans Terlouw</h5>
         </div>
         <div>
-          <button className="IncomingQuestionCard-btn1">
+          <button
+            className="IncomingQuestionCard-btn1"
+            onClick={() =>
+              handleIncomingQuestion("move-incoming-question-to-moderator")
+            }
+          >
             <i className="fa fa-check-double"></i>
             Moderator
           </button>
-          <button className="IncomingQuestionCard-btn2">
+          <button
+            className="IncomingQuestionCard-btn2"
+            onClick={() =>
+              handleIncomingQuestion("move-incoming-question-to-presenter")
+            }
+          >
             <i className="fa fa-user-circle"></i>
             Presenter
           </button>
         </div>
       </div>
       <div className="card-body">
-        <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum.
-        </p>
+        <p>{props.question.questionText}</p>
       </div>
     </section>
   );
